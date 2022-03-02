532a17fe605af80070599787b406067513a72f03
Estudiante <ignmmartin@gmail.com>
2022-03-02 12:26:29 +0000

    Creada app del tutorial

---
+++ src/app/modal/modal.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { DataService, Note } from '../services/data.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+  @Input() id: string;
+  note: Note = null;
+
+  constructor(private dataService: DataService, private modalCtrl: ModalController, private toastCtrl: ToastController) { }
+
+  ngOnInit() {
+    this.dataService.getNoteById(this.id).subscribe(res => {
+      this.note = res;
+    });
+  }
+
+  async deleteNote() {
+    await this.dataService.deleteNote(this.note)
+    this.modalCtrl.dismiss();
+  }
+ 
+  async updateNote() {
+    await this.dataService.updateNote(this.note);
+    const toast = await this.toastCtrl.create({
+      message: 'Note updated!.',
+      duration: 2000
+    });
+    toast.present();
+ 
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+
+    apiKey: "AIzaSyDsk-ceL5k2oLN5Em1jtV8_4LtnKBwEXmQ",
+  
+    authDomain: "miprimerfirebase-ef903.firebaseapp.com",
+  
+    projectId: "miprimerfirebase-ef903",
+  
+    storageBucket: "miprimerfirebase-ef903.appspot.com",
+  
+    messagingSenderId: "493038082225",
+  
+    appId: "1:493038082225:web:1bcb0ba5e6f98bac116a90"
+  
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
